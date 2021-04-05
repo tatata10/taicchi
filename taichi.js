605cca3467d2d0d4d1de999254d11e4e77647f6f
tatata10 <79780942+tatata10@users.noreply.github.com>
2021-04-05 12:57:06 +0900

    Update taichi.js

--- taichi.js
+++ taichi.js
@@ -1,7 +1,7 @@
 
     'use strict';
 
-    const images =['001.jpg','../試作品/002.jpg','../試作品/中村菓子舗3.jpg','../試作品/R2efcc720d4066ec8b3b194005d90a134.jpeg'];
+    const images =['001.jpg','002.jpg','中村菓子舗3.jpg','R2efcc720d4066ec8b3b194005d90a134.jpeg'];
     let current=0;
   
     function changeImages(num) {
